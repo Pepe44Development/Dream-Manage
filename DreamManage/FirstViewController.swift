9003d0ee655601bdf34093753793d655a1def3b5
Pepe44DEV <pascalproessnitz@icloud.com>
2020-07-15 03:36:19 +0200

    Initial Commit

---
+++ DreamManage/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  DreamManage
+//
+//  Created by Pascal Priessnitz on 15.07.20.
+//  Copyright © 2020 Pepe44DEV. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DreamManage/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  DreamManage
+//
+//  Created by Pascal Priessnitz on 15.07.20.
+//  Copyright © 2020 Pepe44DEV. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
